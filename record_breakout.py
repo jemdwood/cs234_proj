f1175f397c2c72f2c5bbe3690417b0e1bebff67a
Jeremy Wood <Jemdwood@gmail.com>
2017-05-24 15:35:56 -0700

    changed score threshold

--- record_breakout.py
+++ record_breakout.py
@@ -32,7 +32,7 @@ except Exception:
 
 
 RECORD_EVERY  = 1 #record every n frames (should be >= 1)
-SCORE_THRESHOLD = 1 #reasonably hard to achieve score. However the score is honestly oddly set up
+SCORE_THRESHOLD = 15 #reasonably hard to achieve score. However the score is honestly oddly set up
 HORIZ_DOWNSAMPLE =  1 # Leave at 1. Other values and you can't see some thin, but critical, parts of the environment
 VERT_DOWNSAMPLE = 1 #1 or 2. I find it harder to do the laser gates when set to 2, but should theoretically be possible
 SPEED = 0 # 0 or 1 at most. I find 1 difficult
