bbc746563b1a526aaf6b8c905626dbe34e43429b
Vincent Sitzmann <sitzmann@cs.stanford.edu>
2017-06-05 16:04:01 -0700

    fixed bug in reward shaping

--- train-atari_pretrain_method.py
+++ train-atari_pretrain_method.py
@@ -234,12 +234,12 @@ def get_config():
             #StartProcOrThread(master),
             PeriodicTrigger(Evaluator(
                 EVAL_EPISODE, ['state'], ['policy'], get_player),
-                every_k_epochs=3),
+                every_k_epochs=1),
         ],
         session_creator=sesscreate.NewSessionCreator(
             config=get_default_sess_config(0.5)),
         steps_per_epoch=int(rec_df.size()/BATCH_SIZE),
-        max_epoch=1000,
+        max_epoch=5,
     )
 
 
